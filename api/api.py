02508c5ee200685b7866df858a5f36e8717b2265
Cornholius <y.layshkin@gmail.com>
2021-08-02 07:42:15 +0300

    куча мелких изменений

--- api/api.py
+++ api/api.py
@@ -1,20 +1,31 @@
+from rest_framework.response import Response
 from .models import Post, Tag
 from rest_framework import viewsets, permissions
 from .serializers import PostSerializer, TagSerializer
 
 
-# Todo ViewSet
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_date')
-    permission_classes = [
-        permissions.AllowAny
-    ]
+    permission_classes = [permissions.AllowAny]
     serializer_class = PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        write_serializer = PostSerializer(data=request.data)
+        write_serializer.is_valid(raise_exception=True)
+        instance = write_serializer.save()
+        read_serializer = PostSerializer(instance)
+        for i in request.data['tags'].split():
+            try:
+                tag = Tag.objects.get(title=i)
+                instance.tags.add(tag)
+            except:
+                new_tag = Tag.objects.create(title=i, slug=i)
+                instance.tags.add(new_tag)
+
+        return Response(read_serializer.data)
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all().order_by('title')
-    permission_classes = [
-        permissions.AllowAny
-    ]
+    permission_classes = [permissions.AllowAny]
     serializer_class = TagSerializer
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+data = {'title': '123', 'text': '123', 'tags': '123 456 789'}
+
+for i in data['tags'].split():
+    print(i)
